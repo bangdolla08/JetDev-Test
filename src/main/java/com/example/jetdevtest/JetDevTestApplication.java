6da99d79bab3b8f1bd7b1c7faccbf1f01fc0a823
bangdolla <bangdolla08@gmail.com>
2023-10-27 05:55:21 +0700

    init project for spring boot

---
+++ src/main/java/com/example/jetdevtest/JetDevTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jetdevtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetDevTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetDevTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jetdevtest/JetDevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jetdevtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetDevTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
